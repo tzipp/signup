4bdc668e87b5870e7abf9cd4cf19d120158ff4e1
tfarrell-vz <farrell@abiresearch.com>
2014-11-13 15:33:08 -0500

    Post renders new page and can unload the post data.

--- signup.py
+++ signup.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import jinja2
 import webapp2
@@ -22,11 +23,22 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_to_string(template, **kw))
 
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+
 class SignupHandler(Handler):
     def get(self):
         self.render('signup.html')
 
     def post(self):
-        self.response.out.write("Posteddd")
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        self.render('post.html', username=username, password=password, verify=verify, email=email)
+
 
 app = webapp2.WSGIApplication([('/signup', SignupHandler)], debug=True)
